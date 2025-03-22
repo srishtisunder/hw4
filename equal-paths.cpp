15a6e357302d02d7968ad1d9526d09948b3b21cd
Srishti Shyam Sunder <sshyamsu@usc.edu>
2025-03-22 09:35:27 +0000

    implemented equal-paths.cpp

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,5 +1,7 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include <string>
+#include <iostream>
 
 #endif
 
@@ -8,11 +10,39 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+void traverseTree(Node* node, int& depth, string pathSoFar, bool& result){
+  if(node==nullptr){return;}
+  if(node->left==nullptr && node->right==nullptr){
+    if((int)pathSoFar.size()!=depth){
+      result=false;
+    }
+    return;
+  }
+
+  traverseTree(node->left, depth, pathSoFar + "L", result);
+  traverseTree(node->right, depth, pathSoFar +"R", result);
+}
+
+int pathLength(Node* node){
+  if(node==nullptr){return 0;}
+  
+  int left = pathLength(node->left);
+  int right = pathLength(node->right);
+  return 1+max(left,right);
+}
 
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if(root == nullptr){
+      return true;
+    }
+    int depth = (pathLength(root) - 1);
+    bool result = true;
+    string pathSoFar = "";
 
+    traverseTree(root, depth, pathSoFar, result);
+
+    return result;
 }
-
